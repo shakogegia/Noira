c40bcaf2f5bed466b4ccb17ceae52f95d6a39a05
Shalva Gegia <shalva.gegia@gmail.com>
2025-09-15 14:40:59 +0200

    Initial Commit

---
+++ Noira/NoiraApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoiraApp.swift
+//  Noira
+//
+//  Created by Shalva Gegia on 15/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct NoiraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
